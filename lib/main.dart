c957dfb63f313364079bf5c86c1071289a7f8021
tanduyfcsc <tanduyfcsc@gmail.com>
2024-08-28 22:16:31 +0700

    Final project, Cam on moi thu

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:video_calling/utils.dart';
+import 'dart:math' as math;
+
+import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
+
+final String localUserId = math.Random().nextInt(10000).toString();
 
 void main() {
   runApp(const MyApp());
@@ -11,40 +17,65 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class HomeScreen extends StatelessWidget {
+  final textEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                  padding: EdgeInsets.all(15),
+                  child: TextFormField(
+                    controller: textEditingController,
+                    decoration: InputDecoration(
+                        labelText: "Join a call by id",
+                        border: OutlineInputBorder()),
+                  )),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              CallScreen(callId: textEditingController.text),
+                        ));
+                  },
+                  child: Text("Join"))
+            ],
+          ),
+        ),
       ),
-      body: Center(),
     );
+  }
+}
+
+class CallScreen extends StatelessWidget {
+  final String callId;
+
+  const CallScreen({super.key, required this.callId});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: ZegoUIKitPrebuiltCall(
+            appID: Utils.appId,
+            appSign: Utils.appSignin,
+            userID: localUserId,
+            userName: 'user_$localUserId',
+            callID: callId,
+            config: ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall()));
   }
 }
